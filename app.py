d000506eb1fb7556b754a410aaf9f89a7e1d03f2
ArthurSarturi <seyycookie@gmail.com>
2025-08-24 14:51:48 -0300

    CHORE: criando main.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from routes.bookroutes import book_bp
+from db import db
+
+app = Flask(__name__)
+app.register_blueprint(book_bp)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
